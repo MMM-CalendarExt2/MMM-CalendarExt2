3c02a297913e2487d86b2e27f2136ef913f170de
Seongnoh Sean Yi <eouia0819@gmail.com>
2020-01-28 14:47:31 +0100

    urgent fix for re-scan issue on network problem

--- node_helper.js
+++ node_helper.js
@@ -86,10 +86,10 @@ module.exports = NodeHelper.create({
         cb(calendar, null, e)
       } else {
         cb(calendar, data, e)
-        setTimeout(()=>{
-          this.scanCalendar(calendar, cb)
-        }, calendar.scanInterval)
       }
+      setTimeout(()=>{
+        this.scanCalendar(calendar, cb)
+      }, calendar.scanInterval)
     })
   },
 
