6af95b5f809a31994476193171e345c464540299
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-27 01:38:50 +0100

    == -> ===

--- CALEXT2_Slot.js
+++ CALEXT2_Slot.js
@@ -76,7 +76,7 @@ class Slot {
           event.startHere = true;
         if (eE.isBetween(this.start, this.end, null, "(])"))
           event.endHere = true;
-        if (eE.format("HHmmss") == "000000")
+        if (eE.format("HHmmss") === "000000")
           event.endDate = moment(eE).add(-1, "second").endOf("day").format("X");
         this.events.push(event);
       }
