b1d411f624a4405a2d320012cc49bcf0828fb4f9
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-10-28 18:24:41 +0100

    Switch to flat config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,13 +1,15 @@
-{
+const config = {
   "extends": ["stylelint-config-standard"],
   "plugins": ["stylelint-prettier"],
   "root": true,
   "rules": {
     "declaration-block-no-redundant-longhand-properties": null,
+    "keyframes-name-pattern": "^(MMM-CalendarExt2|CX2|fake_module|module-content|[a-z][a-zA-Z0-9]+(_\\d+)?)$",
     "no-descending-specificity": null,
     "prettier/prettier": true,
     "rule-empty-line-before": null,
-    "selector-class-pattern": "^(MMM-CalendarExt2|CX2|fake_module|module-content|[a-z][a-zA-Z0-9]+(_\\d+)?)$",
-    "keyframes-name-pattern": "^(MMM-CalendarExt2|CX2|fake_module|module-content|[a-z][a-zA-Z0-9]+(_\\d+)?)$"
+    "selector-class-pattern": "^(MMM-CalendarExt2|CX2|fake_module|module-content|[a-z][a-zA-Z0-9]+(_\\d+)?)$"
   }
-}
+};
+
+export default config;
