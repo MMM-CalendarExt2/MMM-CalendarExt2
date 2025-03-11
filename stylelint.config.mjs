ac9a6a996d56cf733268de4f27b52b6cd682d2fa
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-03-11 18:38:46 +0100

    Simplify stylelint-prettier config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,13 +1,11 @@
 const pattern = "^(MMM-CalendarExt2|CX2|fake_module|module-content|[a-z][a-zA-Z0-9]+(_\\d+)?)$";
 const config = {
-  "extends": ["stylelint-config-standard"],
-  "plugins": ["stylelint-prettier"],
+  "extends": ["stylelint-config-standard", "stylelint-prettier/recommended"],
   "root": true,
   "rules": {
     "declaration-block-no-redundant-longhand-properties": null,
     "keyframes-name-pattern": pattern,
     "no-descending-specificity": null,
-    "prettier/prettier": true,
     "rule-empty-line-before": null,
     "selector-class-pattern": pattern
   }
